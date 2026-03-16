1df3bf38e0f194812fa2b3172274bc5c2240ca31
test01 - MinhNguyen <nguyenminh2707@gmail.com>
2026-03-16 11:36:29 +0700

    feat: done challenge04.2

--- tests/lesson-04/moon-kingdom.js
+++ tests/lesson-04/moon-kingdom.js
@@ -28,4 +28,33 @@ function createCharacters() {
     console.log("Posible winners are:", possibleWinners);
 }
 
+// call the function to see the result
 createCharacters();
+
+/* Tạo hàm printLeaderboard:
+● Nhận vào tham số: players là mảng các object: [{name: "Mario", score:
+1000}, ...]
+● Sắp xếp mảng người chơi theo thứ tự score từ cao đến thấp.
+● In ra bảng xếp hạng. Lưu ý: với 3 vị trí 1, 2, 3, hãy thêm huy chương phía
+trước (🥇, 🥈, 🥉) */
+
+const players = [
+    { name: "Mario", score: 1000 },
+    { name: "Mint", score: 0 },
+    { name: "Kristian", score: 1111 },
+    { name: "Annie", score: 2999 },
+    { name: "Dan Wooden", score: 777 },
+    { name: "Hoang Nguyen", score: 123 },
+];
+
+function printLeaderboard(players) {
+    const sortedPlayers = players.sort((a, b) => b.score - a.score);
+    sortedPlayers[0].name = "🥇 " + sortedPlayers[0].name;
+    sortedPlayers[1].name = "🥈 " + sortedPlayers[1].name;
+    sortedPlayers[2].name = "🥉 " + sortedPlayers[2].name;
+
+    //print out all players
+    console.log(players);
+}
+
+printLeaderboard(players);
